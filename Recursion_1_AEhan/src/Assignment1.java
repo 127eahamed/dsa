f3ffa9313f0c20b456f83f1544d176024e2a7513
Ehan <127eahamed@frhsd.com>
2025-10-06 08:48:28 -0400

    add assignment1 and configuration files for recursive project thing

---
+++ Recursion_1_AEhan/src/Assignment1.java
@@ -0,0 +1,40 @@
+public class Assignment1 {
+    public static void strictlyIncreasingDigits(int digits) {
+        if (digits > 9) {
+            System.out.println("Strictly increasing numbers cannot exceed 9 digits");
+            return;
+        }
+
+        printNumbers(10 * digits, digits);
+    }
+    public static void printNumbers(int number, int digits) {
+        if (number >= (10 * (digits + 1))) {
+            return;
+        }
+
+        System.out.println(nextNumber(number, digits));
+        printNumbers(number + 1, digits);
+    }
+    public static int nextNumber(int number, int digits) {
+        if (areDigitsIncreasing(number)) {
+            return number;
+        } else {
+            return nextNumber(number + 1, digits);
+        }
+    }
+    public static boolean areDigitsIncreasing(int number) {
+        String digitsString = "" + number;
+        for (int index = 0; index < digitsString.length() - 1; index++) {
+            int currentDigit = Integer.parseInt(
+                digitsString.substring(index, index + 1)
+            );
+            int nextDigit = Integer.parseInt(
+                digitsString.substring(index + 1, index + 2)
+            );
+            if (!(nextDigit > currentDigit)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Recursion_1_AEhan/src/AssignmentsCLI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AssignmentsCLI {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("""
+Choose an assignment:
+1: Strictly Increasing Digits Generator
+2: m-th Summation of the First n Natural Numbers
+3: Generate Binary Strings Without Consecutive 1's
+4: Print n-Digit Binary Numbers with Equal Sum in Left and Right Halves
+5: Calculate Maximum Number of Chocolates You Can Eat
+
+Pick an assignment to run: (Number or Name)
+""");
+
+    }
+}
