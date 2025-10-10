743d648b6259002cdb2321ca7294d22f27fb16f0
Ehan <127eahamed@frhsd.com>
2025-10-10 10:29:26 -0400

    implement assignment 4, move main method

--- Recursion_1_AEhan.java
+++ Recursion_1_AEhan.java
@@ -2,67 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Recursion_1_AEhan {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("""
-Choose an assignment:
-1: Strictly Increasing Digits Generator
-2: m-th Summation of the First n Natural Numbers
-3: Generate Binary Strings Without Consecutive 1's
-4: Print n-Digit Binary Numbers with Equal Sum in Left and Right Halves
-5: Calculate Maximum Number of Chocolates You Can Eat
-
-Pick assignment to run (number or name):\s""");
-        String input = sc.nextLine().toLowerCase();
-        if (
-            (input.contains("1") && !(
-                input.contains("consecutive") || input.contains("1's")
-            )) ||
-            input.contains("strictly") ||
-            input.contains("increasing")
-        ) {
-            System.out.print("""
-
-Assignment 1: Strictly Increasing Digits Generator
-
-Enter number of digits:\s"""
-            );
-            strictlyIncreasingDigits(sc.nextInt());
-        } else if (
-            input.contains("2") ||
-            input.contains("summation") ||
-            input.contains("natural")
-        ) {
-            System.out.print("""
-
-Assignment 2: m-th Summation of the First n Natural Numbers
-
-Enter n, number of digits:\s"""
-            );
-        } else if (
-            input.contains("3") ||
-            input.contains("without") ||
-            input.contains("consecutive") ||
-            input.contains("1's")
-        ) {
-            System.out.print("""
-
-Assignment 3: Generate Binary Strings Without Consecutive 1's
-
-Enter k, size:\s"""
-            );
-            genBinaryStringsWithoutConsecutive1s(sc.nextInt());
-        }
-    }
-
-    public static void strictlyIncreasingDigits(int digits) {
-        if (digits > 9) {
-            System.out.println("Strictly increasing numbers cannot exceed 9 digits");
+    /*
+     * Assignment 1: Recursive Strictly Increasing Digits Generator
+     * */
+    public static void strictlyIncreasingDigits(int n) {
+        if (n > 9) {
             return;
         }
 
-        printNumbers((int) Math.pow(10, digits), digits);
+        printNumbers((int) Math.pow(10, n), n);
     }
+
     public static void printNumbers(int number, int digits) {
         if (number >= (Math.pow(10, digits + 1))) {
             return;
@@ -94,6 +44,14 @@ Enter k, size:\s"""
         return true;
     }
 
+    /*
+     * Assignment 2: mth Summation of the First n Natural Numbers
+     * */
+
+
+    /*
+     * Assignment 3: Generate Binary Strings Without Consecutive 1's
+     * */
     public static void genBinaryStringsWithoutConsecutive1s(int k) {
         ArrayList<String> startingStrings = new ArrayList<>();
         startingStrings.add("0");
@@ -120,5 +78,122 @@ Enter k, size:\s"""
             }
         }
         return binaryStringsWithoutConsecutive1s(newStrings, k);
+    }
+
+    /*
+     * Assignment 4: Print n-Digit Binary Numbers with Equal Sum in Left and Right Halves
+     * */
+    public static void printNDigitBinaryNumsWithEqualSum(int n) {
+        printNDigitBinaryNumsWithEqualSumFromString("", n);
+    }
+    public static void printNDigitBinaryNumsWithEqualSumFromString(
+        String binaryString, int n
+    ) {
+        if (binaryString.length() == n) {
+            char[] leftHalf = binaryString.substring(
+                0, n / 2
+            ).toCharArray();
+            char[] rightHalf = binaryString.substring(
+                (int) (((double) n / 2) + 0.5)
+            ).toCharArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            for (char c : leftHalf) {
+                if (c == '1') {
+                    leftSum++;
+                }
+            }
+            for (char c : rightHalf) {
+                if (c == '1') {
+                    rightSum++;
+                }
+            }
+            if (leftSum == rightSum) {
+                System.out.println(binaryString);
+            }
+        } else {
+            printNDigitBinaryNumsWithEqualSumFromString(
+                binaryString + "0",
+                n
+            );
+            printNDigitBinaryNumsWithEqualSumFromString(
+                binaryString + "1",
+                n
+            );
+        }
+    }
+
+
+    /*
+     * Assignment 5: Calculate Maximum Number of Chocolates You Can Eat
+     * */
+
+
+    /**
+     * Main Method to Test/Run Each Assignment
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("""
+Choose an assignment:
+1: Strictly Increasing Digits Generator
+2: m-th Summation of the First n Natural Numbers
+3: Generate Binary Strings Without Consecutive 1's
+4: Print n-Digit Binary Numbers with Equal Sum in Left and Right Halves
+5: Calculate Maximum Number of Chocolates You Can Eat
+
+Pick assignment to run (number or name):\s""");
+        String input = sc.nextLine().toLowerCase();
+        if (
+            (input.contains("1") && !(
+                input.contains("consecutive") || input.contains("1's")
+            )) ||
+            input.contains("strictly") ||
+            input.contains("increasing")
+        ) {
+            System.out.print(
+                "\n" +
+                "Assignment 1: Strictly Increasing Digits Generator" +
+                "\n" +
+                "Enter number of digits: "
+            );
+            strictlyIncreasingDigits(sc.nextInt());
+        } else if (
+            input.contains("2") ||
+            input.contains("summation") ||
+            input.contains("natural")
+        ) {
+            System.out.print(
+                "\n" +
+                "Assignment 2: m-th Summation of the First n Natural Numbers" +
+                "\n" +
+                "Enter n, number of digits: "
+            );
+        } else if (
+            input.contains("3") ||
+            input.contains("without") ||
+            input.contains("consecutive") ||
+            input.contains("1's")
+        ) {
+            System.out.print(
+                "\n" +
+                "Assignment 3: Generate Binary Strings Without Consecutive 1's\n" +
+                "\n" +
+                "Enter k, size: "
+            );
+            genBinaryStringsWithoutConsecutive1s(sc.nextInt());
+        } else if (
+            input.contains("4") ||
+            input.contains("equal") ||
+            input.contains("halves")
+        ) {
+            System.out.print(
+                "\n" +
+                "Assignment 3: Generate Binary Strings Without Consecutive 1's\n" +
+                "\n" +
+                "Enter k, size: "
+            );
+            printNDigitBinaryNumsWithEqualSum(sc.nextInt());
+        }
     }
 }
